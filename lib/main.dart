8a5e127ff909b2611b73ec720e3bda05459dcaff
Nelson Alexandrino <nasson.alexander@gmail.com>
2019-08-25 20:43:40 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,293 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[HomeScreenTopPart(), HomeScreenBottomPart()],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home, color: Color(0xFFE52020)),
+              title: Text(
+                'Home',
+                style: TextStyle(color: Color(0xFFE52020)),
+              )),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search), title: Text('Search')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.bookmark), title: Text('Bookmark')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline), title: Text('Profile')),
+        ],
+      ),
+    );
+  }
+}
+
+class HomeScreenTopPart extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      height: 420.0,
+      child: Stack(
+        children: <Widget>[
+          ClipPath(
+            clipper: Mclipper(),
+            child: Container(
+              height: 370.0,
+              decoration: BoxDecoration(color: Colors.white, boxShadow: [
+                BoxShadow(
+                    color: Colors.black12,
+                    offset: Offset(0.0, 10.0),
+                    blurRadius: 10.0)
+              ]),
+              child: Stack(
+                children: <Widget>[
+                  Image.asset(
+                    'assets/endgame.jpg',
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                    height: 370.0,
+                  ),
+                  Container(
+                    height: double.infinity,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            colors: [
+                          const Color(0x00000000),
+                          const Color(0xD9333333)
+                        ],
+                            stops: [
+                          0.0,
+                          0.9
+                        ],
+                            begin: FractionalOffset(0.0, 0.0),
+                            end: FractionalOffset(0.0, 1.0))),
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 120.0, left: 95.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text('O MAIS ESPERADO DO ANO',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15.0,
+                                  fontFamily: 'SF-Pro-Display-Bold',
+                                  fontWeight: FontWeight.bold)),
+                          Text('Avengers: Endgame',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 45.0,
+                                  fontFamily: 'SF-Pro-Display-Bold',
+                                  fontWeight: FontWeight.bold))
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            top: 370.0,
+            right: -20.0,
+            child: FractionalTranslation(
+              translation: Offset(0.0, -0.5),
+              child: Row(
+                children: <Widget>[
+                  FloatingActionButton(
+                    backgroundColor: Colors.white,
+                    onPressed: () {},
+                    child: Icon(
+                      Icons.add,
+                      color: Color(0xFFE52020),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 12.0,
+                  ),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(30.0),
+                    child: RaisedButton(
+                      color: Color(0xFFE52020),
+                      onPressed: () {},
+                      padding: EdgeInsets.symmetric(
+                          vertical: 15.0, horizontal: 80.0),
+                      child: Row(
+                        children: <Widget>[
+                          Text(
+                            'Assista agora',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15.0,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            width: 5.0,
+                          ),
+                          Icon(
+                            Icons.arrow_forward,
+                            color: Colors.white,
+                            size: 18.0,
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class HomeScreenBottomPart extends StatelessWidget {
+  List<String> images = [
+    'assets/infinity.png',
+    'assets/blackpanther.png',
+    'assets/aquaman.png'
+  ];
+
+  List<String> titles = ['Avengers: infinity war', 'Black Panther', 'Aquaman'];
+
+  List<Widget> movies() {
+    List<Widget> movieList = new List();
+
+    for (int i = 0; i < 3; i++) {
+      var movieItem = Padding(
+        padding: EdgeInsets.symmetric(vertical: 25.0, horizontal: 12.0),
+        child: Container(
+          height: 220.0,
+          width: 135.0,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20.0),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black12,
+                    blurRadius: 10.0,
+                    offset: Offset(0.0, 10.0))
+              ]),
+          child: Column(
+            children: <Widget>[
+              ClipRRect(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20.0),
+                    topRight: Radius.circular(20.0)),
+                child: Image.asset(
+                  images[i],
+                  width: double.infinity,
+                  height: 130.0,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 4.0, left: 8.0, right: 8.0),
+                child: Text(
+                  titles[i],
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 3.0),
+                child: Text(i == 0 ? "Season 2" : ""),
+              )
+            ],
+          ),
+        ),
+      );
+      movieList.add(movieItem);
+    }
+
+    return movieList;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      height: 360.0,
+      margin: EdgeInsets.only(left: 65.0),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'Assista agora',
+                  style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
+                ),
+                FlatButton(
+                  child: Text('Veja mais'),
+                  onPressed: () {},
+                )
+              ],
+            ),
+          ),
+          Container(
+            height: 250,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: movies(),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Mclipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = new Path();
+    path.lineTo(0.0, size.height - 100.0);
+
+    var controlpoint = Offset(35.0, size.height);
+    var endpoint = Offset(size.width / 2, size.height);
+
+    path.quadraticBezierTo(
+        controlpoint.dx, controlpoint.dy, endpoint.dx, endpoint.dy);
+
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, 0.0);
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return null;
+  }
+}
